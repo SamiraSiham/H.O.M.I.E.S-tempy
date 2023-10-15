fd767076b683c056875847d585bb727766338098
SamiraSiham <samirasiham.school2021@gmail.com>
2023-10-15 18:47:51 +0100

    basic functioning

--- src/index.js
+++ src/index.js
@@ -1,64 +1,90 @@
-import {config} from 'dotenv';
-import {Client,Collection} from 'discord.js';
+import { config } from "dotenv";
+import { Client, Collection } from "discord.js";
 // const {ChannelType} = require('discord.js');
-import { ChannelType } from 'discord.js';
+import {
+  ChannelType,
+  PermissionFlagsBits,
+} from "discord.js";
 
 config();
 
-
-
 const client = new Client({
-    intents: ["Guilds" , "GuildMessages", "GuildMembers", "GuildVoiceStates"],
+  intents: ["Guilds", "GuildMessages", "GuildMembers", "GuildVoiceStates"],
 });
 
-
-client.on("ready", async()=>{
-    console.log(`bot is on ${client.user.tag}`);
+client.on("ready", async () => {
+  console.log(`${client.user.tag} is online !`);
 });
 
-let voiceManager = new Collection()
+let voiceManager = new Collection();
 
-client.on('voiceStateUpdate', async(oldState, newState)=>{
-    const {member, guild} = oldState;
-    const newChannel = newState.channel;
-    const oldChannel = oldState.channel;
-    const join_to_create = "1162773476516560986";
+client.on("voiceStateUpdate", async (oldState, newState) => {
+  const { member, guild } = oldState;
+  const newChannel = newState.channel;
+  const oldChannel = oldState.channel;
+  const join_to_create = "1162773476516560986";
 
-    // if user joins voice channel
-    if(oldChannel !== newChannel && newChannel.id === join_to_create){
-        const voiceChannel = await guild.channels.create({
-            name : `${member.user.username}`,
-            type : ChannelType.GuildVoice,
-            parent: newChannel.parent,
-            PermissionOverWrites:[
-                {
-                    id : member.id,
-                    allow: ['CONNECT','MANAGE_CHANNELS']
-                },
-                {
-                    id: guild.id,
-                    allow: ['CONNECT']
-                }
-            ]
-        });
+  // if user joins voice channel
+  if (
+    oldChannel !== newChannel &&
+    newChannel &&
+    newChannel.id === join_to_create
+  ) {
+    const voiceChannel = await guild.channels.create({
+      name: `${member.user.username}`,
+      type: ChannelType.GuildVoice,
+      parent: newChannel.parent,
+      permissionOverwrites: [
+        {
+          id: member.id,
+          allow: [
+            PermissionFlagsBits.Connect,
+            PermissionFlagsBits.ReadMessageHistory,
+            PermissionFlagsBits.Speak,
+            PermissionFlagsBits.AddReactions,
+            PermissionFlagsBits.AttachFiles,
+            PermissionFlagsBits.CreateInstantInvite,
+            PermissionFlagsBits.KickMembers,
+            PermissionFlagsBits.UseSoundboard,
+            PermissionFlagsBits.Stream,
+            PermissionFlagsBits.UseVAD,
+            PermissionFlagsBits.UseApplicationCommands,
+            PermissionFlagsBits.UseEmbeddedActivities,
+            PermissionFlagsBits.UseExternalEmojis,
+            PermissionFlagsBits.UseExternalStickers
+          ],
+          deny: [PermissionFlagsBits.ManageChannels],
+        },
+        {
+          id: guild.id,
+          allow: [PermissionFlagsBits.ViewChannel, PermissionFlagsBits.Connect],
+        },
+      ],
+    });
+    voiceManager.set(member.id, voiceChannel.id);
 
-        voiceManager.set(member.id,voiceChannel.id);
+    // spam protection
+    // await newChannel.permissionOverwrites.edit(member, { Connect: false });
+    // setTimeout(() => {
+    //   newChannel.permissionOverwrites.delete(member);
+    // }, 3 * 1000);
+    return setTimeout(async () => {
+      member.voice.setChannel(voiceChannel);
+    }, 600);
+  }
+  // if user leaves or switches channel
+  const JTCCHANNEL = voiceManager.get(member.id);
+  if (
+    JTCCHANNEL &&
+    oldChannel.id === JTCCHANNEL &&
+    (!newChannel || newChannel.id !== JTCCHANNEL)
+  ) {
+    // console.log(JTCCHANNEL);
+    voiceManager.set(member.id, null);
+    oldChannel.delete().catch((e) => null);
+  }
+});
 
-        // spam protection
-        await newChannel.permissionOverwrites.edit(member,{Connect : false})
-        setTimeout(()=>{
-            newChannel.permissionOverwrites.delete(member)
-        }, 30*1000);
-        return setTimeout(()=>{
-            member.voice.setChannel(voiceChannel);
-        }, 600);
-    }else{
-        console.log(newChannel.id);
-    }
-})
-
-
-
-client.login(process.env.TOKEN).catch((e)=>{
-    console.log(e);
+client.login(process.env.TOKEN).catch((e) => {
+  console.log(e);
 });
